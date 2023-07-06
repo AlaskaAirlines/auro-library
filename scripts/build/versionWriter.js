047b2edf1ceceb6c841019691ae4c66e4c5dd929
Isaac <isaac.ortega@alaskaair.com>
2023-07-06 08:49:11 -0700

    chore: fix lint errors for versionWriter.js

--- scripts/build/versionWriter.js
+++ scripts/build/versionWriter.js
@@ -3,19 +3,23 @@
 
 // ---------------------------------------------------------------------
 
-function writeDepVersionFile(pkg) {
-  console.warn('writeDepVersionFile', pkg);
+const auroSubNameIndex = 5;
 
+/**
+ * Writes a version file for the specified dependency package into the `src` directory.
+ * @param {string} pkg Dependency to write version file for.
+ */
+function writeDepVersionFile(pkg) {
   const fs = require('fs');
 
-  const path = '../node_modules/' + pkg + '/package.json';
+  const path = `../node_modules/${pkg}/package.json`;
   const json = require(path);
-  const version = json.version;
-  const elemSubName = pkg.substring(pkg.indexOf('auro-') + 5);
-  const versionFilePath = './src/' + elemSubName + 'Version.js';
+  const {version} = json;
+  const elemSubName = pkg.substring(pkg.indexOf('auro-') + auroSubNameIndex);
+  const versionFilePath = `./src/${elemSubName}Version.js`;
 
   fs.writeFileSync(versionFilePath, `export default '${version}'`);
-};
+}
 
 // add the code below
 module.exports = { writeDepVersionFile };
