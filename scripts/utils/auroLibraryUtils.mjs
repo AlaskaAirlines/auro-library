4cee74b39cd19d8be139fa715a22b1638954ca65
Jordan Jones <jmjones2463@gmail.com>
2023-12-28 14:10:06 -0800

    feat(closest): add closest element function #43

--- scripts/utils/auroLibraryUtils.mjs
+++ scripts/utils/auroLibraryUtils.mjs
@@ -174,5 +174,21 @@ export default class AuroLibraryUtils {
      */
     fs.writeFileSync(destination, result, { encoding: 'utf8'});
   }
+
+  /**
+   * Finds and returns the closest HTML Element based on a selector.
+   */
+  closestElement(
+    selector, // selector like in .closest()
+    base = this, // extra functionality to skip a parent
+    __Closest = (el, found = el && el.closest(selector)) =>
+      !el || el === document || el === window
+        ? null // standard .closest() returns null for non-found selectors also
+        : found
+          ? found // found a selector INside this element
+          : __Closest(el.getRootNode().host) // recursion!! break out to parent DOM
+  ) {
+    return __Closest(base);
+  }
 }
 
