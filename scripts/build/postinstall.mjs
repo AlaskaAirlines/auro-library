d8eed9ffb985dd57bbd179e7f2a1bbcbbd587b8f
Dale Sande <dale.sande@gmail.com>
2024-01-09 12:40:58 -0800

    fix: update relative path
    Fix to package.json relative ref, add additional dir bump.
    
    Changes to be committed:
    modified:   scripts/build/postinstall.mjs

--- scripts/build/postinstall.mjs
+++ scripts/build/postinstall.mjs
@@ -3,7 +3,7 @@
 import chalk from 'chalk';
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
-const pjson = require('../package.json');
+const pjson = require('../../package.json');
 
 console.log(chalk.hex('#f26135')(`
 
@@ -21,7 +21,7 @@ console.log(chalk.hex('#f26135')(`
 ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
 
         Thanks for installing the latest version
-                of `) + chalk.hex('#ffd200').bold(`${pjson.name} v${pjson.version}.`) + chalk.hex('#f26135')(`
+        of `) + chalk.hex('#ffd200').bold(`${pjson.name} v${pjson.version}.`) + chalk.hex('#f26135')(`
 
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
