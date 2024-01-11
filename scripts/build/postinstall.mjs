e8cdbf8114dcc1e8a9a2af0e37fdca7a5616f6be
Dale Sande <dale.sande@gmail.com>
2024-01-11 10:06:05 -0800

    fix: update package location ref
    Changes to be committed:
    modified:   scripts/build/postinstall.mjs

--- scripts/build/postinstall.mjs
+++ scripts/build/postinstall.mjs
@@ -3,7 +3,7 @@
 import chalk from 'chalk';
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
-const pjson = require('../../package.json');
+const pjson = require('../package.json');
 
 console.log(chalk.hex('#f26135')(`
 
