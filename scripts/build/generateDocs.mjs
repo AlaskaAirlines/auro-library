aff0f158208855a548878db3cefe30d3e7fd2a28
Doug <doug@dougflynn.dev>
2024-10-31 14:47:25 -0700

    refactor: extract all config/function calls from docs scripts
    This commit adds a new directory called `scripts/build/processors/*` which contains pre-configured processor files, and then imports those files into matching `scripts/build/*` files that will be run as actual scripts.

--- scripts/build/generateDocs.mjs
+++ scripts/build/generateDocs.mjs
@@ -1,80 +1,20 @@
+// ------------------------------------------------------
+// Docs Generation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// This script will generate the docs for the component
+// based on example HTML, markdown files, and more.
+//
+// The actions in this file live in:
+// scripts/build/processors/defaultDocsProcessor.mjs
+//
+// This script is intended to be run AS-IS without modification.
+// To run a different processor, please see the defaultDocsProcessor.mjs file
+// and re-create in your repo OR add a new processor file.
+// ------------------------------------------------------
+
 import {Logger} from "../utils/logger.mjs";
-import {
-  fromAuroComponentRoot,
-  generateReadmeUrl,
-  processContentForFile,
-  templateFiller
-} from "../utils/sharedFileProcessorUtils.mjs";
-
-// Finally, the main function
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-/**
- * Processor config object.
- * @typedef {Object} ProcessorConfig
- * @property {boolean} [overwriteLocalCopies=true] - The release version tag to use instead of master.
- * @property {string} [remoteReadmeVersion="master"] - The release version tag to use instead of master.
- * @property {string} [remoteReadmeVariant=""] - The variant string to use for the README source.
- * (like "_esm" to make README_esm.md).
- */
-
-/**
- * @param {ProcessorConfig} config - The configuration for this processor.
- * @returns {import('../utils/sharedFileProcessorUtils').FileProcessorConfig[]}
- */
-export const fileConfigs = ({remoteReadmeVersion, remoteReadmeVariant, overwriteLocalCopies}) => [
-  // README.md
-  {
-    identifier: 'README.md',
-    input: {
-      remoteUrl: generateReadmeUrl(remoteReadmeVersion, remoteReadmeVariant),
-      fileName: fromAuroComponentRoot(`/docTemplates/README.md`),
-      overwrite: overwriteLocalCopies
-    },
-    output: fromAuroComponentRoot("/README.md")
-  },
-  // index.md
-  {
-    identifier: 'index.md',
-    input: fromAuroComponentRoot("/docs/partials/index.md"),
-    output: fromAuroComponentRoot("/demo/index.md"),
-    mdMagicConfig: {
-      output: {
-        directory: fromAuroComponentRoot("/demo")
-      }
-    }
-  },
-  // api.md
-  {
-    identifier: 'api.md',
-    input: fromAuroComponentRoot("/docs/partials/api.md"),
-    output: fromAuroComponentRoot("/demo/api.md"),
-    postProcessors: [templateFiller.formatApiTable],
-  }
-];
-
-/**
- *
- * @param {ProcessorConfig} config - The configuration for this processor.
- * @return {Promise<void>}
- */
-export async function processDocFiles(config = {
-  overwriteLocalCopies: true,
-  remoteReadmeVersion: "master",
-  readmeVariant: ""
-}) {
-  // setup
-  await templateFiller.extractNames();
-
-  for (const fileConfig of fileConfigs(config)) {
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      await processContentForFile(fileConfig);
-    } catch (err) {
-      Logger.error(`Error processing ${fileConfig.identifier}: ${err.message}`);
-    }
-  }
-}
+import {processDocFiles} from "./processors/defaultDocsProcessor.mjs";
 
 processDocFiles().then(() => {
   Logger.log('Docs processed successfully');
