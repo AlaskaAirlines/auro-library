9748c11a74be5d7590ab47a5d258b2aa505a2b57
Stephen Rios <Stephen.Rios@AlaskaAir.com>
2025-04-16 10:34:21 -0500

    refactor: convert dateFormatter to class and update parseDate to use a reducer and cleaner regex (code cleanup)

--- scripts/runtime/dateUtilities/dateFormatters.mjs
+++ scripts/runtime/dateUtilities/dateFormatters.mjs
@@ -1,105 +1,102 @@
-const dateFormatter = {
+class dateFormatter {
 
-  /**
-   * Parses a date string into its components.
-   * @param {string} dateStr - Date string to parse.
-   * @param {string} format - Date format to parse.
-   * @returns {Object|undefined}
-   */
-  parseDate: (dateStr, format = 'mm/dd/yyyy') => {
+  constructor() {
 
-    // Guard Clause: Date string is defined
-    if (!dateStr) {
-      return undefined;
-    }
+    /**
+     * @description Parses a date string into its components.
+     * @param {string} dateStr - Date string to parse.
+     * @param {string} format - Date format to parse.
+     * @returns {Object<key["month" | "day" | "year"]: number>|undefined}
+     */
+    this.parseDate = (dateStr, format = 'mm/dd/yyyy') => {
 
-    // Assume the separator is a "/" a defined in our code base
-    const separator = '/';
+      // Guard Clause: Date string is defined
+      if (!dateStr) {
+        return undefined;
+      }
 
-    // Get the parts of the date and format
-    const valueParts = dateStr.split(separator);
-    const formatParts = format.split(separator);
+      // Assume the separator is a "/" a defined in our code base
+      const separator = '/';
 
-    // Check if the value and format have the correct number of parts
-    if (valueParts.length !== formatParts.length) {
-      throw new Error('AuroDatepickerUtilities | parseDate: Date string and format length do not match');
-    }
+      // Get the parts of the date and format
+      const valueParts = dateStr.split(separator);
+      const formatParts = format.split(separator);
 
-    // Holds the result to be returned
-    const result = {
-      day: undefined,
-      month: undefined,
-      year: undefined
+      // Check if the value and format have the correct number of parts
+      if (valueParts.length !== formatParts.length) {
+        throw new Error('AuroDatepickerUtilities | parseDate: Date string and format length do not match');
+      }
+
+      // Holds the result to be returned
+      const result = formatParts.reduce((acc, part, index) => {
+        const value = valueParts[index];
+
+        if ((/m/iu).test(part)) {
+          acc.month = value;
+        } else if ((/d/iu).test(part)) {
+          acc.day = value;
+        } else if ((/y/iu).test(part)) {
+          acc.year = value;
+        }
+
+        return acc;
+      }, {});
+
+      // If we found all the parts, return the result
+      if (result.day && result.month && result.year) {
+        return result;
+      }
+
+      // Throw an error to let the dev know we were unable to parse the date string
+      throw new Error('AuroDatepickerUtilities | parseDate: Unable to parse date string');
     };
 
-    // Iterate over the format and value parts and assign them to the result
-    formatParts.forEach((formatPart, index) => {
-
-      const valuePart = valueParts[index];
-
-      if (formatPart.toLowerCase().match("m")) {
-        result.month = valuePart;
-      } else if (formatPart.toLowerCase().match("d")) {
-        result.day = valuePart;
-      } else if (formatPart.toLowerCase().match("y")) {
-        result.year = valuePart;
-      }
+    /**
+     * Convert a date object to string format.
+     * @param {Object} date - Date to convert to string.
+     * @returns {Object} Returns the date as a string.
+     */
+    this.getDateAsString = (date) => date.toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
     });
 
-    // If we found all the parts, return the result
-    if (result.day && result.month && result.year) {
-      return result;
-    }
+    /**
+     * Converts a date string to a North American date format.
+     * @param {String} dateStr - Date to validate.
+     * @param {String} format - Date format to validate against.
+     * @returns {Boolean}
+     */
+    this.toNorthAmericanFormat = (dateStr, format) => {
 
-    // Throw an error to let the dev know we were unable to parse the date string
-    throw new Error('AuroDatepickerUtilities | parseDate: Unable to parse date string');
-  },
+      if (format === 'mm/dd/yyyy') {
+        return dateStr;
+      }
 
-  /**
-   * Convert a date object to string format.
-   * @param {Object} date - Date to convert to string.
-   * @returns {Object} Returns the date as a string.
-   */
-  getDateAsString: (date) => date.toLocaleDateString(undefined, {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  }),
+      const parsedDate = this.parseDate(dateStr, format);
 
-  /**
-   * Converts a date string to a North American date format.
-   * @param {String} dateStr - Date to validate.
-   * @param {String} format - Date format to validate against.
-   * @returns {Boolean}
-   */
-  toNorthAmericanFormat: (dateStr, format) => {
+      if (!parsedDate) {
+        throw new Error('AuroDatepickerUtilities | toNorthAmericanFormat: Unable to parse date string');
+      }
 
-    if (format === 'mm/dd/yyyy') {
-      return dateStr;
-    }
+      const { month, day, year } = parsedDate;
 
-    const parsedDate = this.parseDate(dateStr, format);
+      const dateParts = [];
+      if (month) {
+        dateParts.push(month);
+      }
 
-    if (!parsedDate) {
-      throw new Error('AuroDatepickerUtilities | toNorthAmericanFormat: Unable to parse date string');
-    }
+      if (day) {
+        dateParts.push(day);
+      }
 
-    const { month, day, year } = parsedDate;
+      if (year) {
+        dateParts.push(year);
+      }
 
-    const dateParts = [];
-    if (month) {
-      dateParts.push(month);
-    }
-
-    if (day) {
-      dateParts.push(day);
-    }
-
-    if (year) {
-      dateParts.push(year);
-    }
-
-    return dateParts.join('/');
+      return dateParts.join('/');
+    };
   }
 };
 
